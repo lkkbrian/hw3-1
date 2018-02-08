42b62ca9312526ee05af4aad233690b58db778dd
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-02-07 20:06:12 -0800

    Create easing.js

---
+++ easing.js
@@ -0,0 +1,12 @@
+function setup(){
+  createCanvas(340, 340);
+}
+
+function draw (){
+  background (255);
+  noStroke();
+  fill(250, 0, 0);
+  ellipse(mouseX, mouseY, 20, 20)
+}
+setup();
+draw ();
