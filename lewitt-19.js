347cfc0057aac81bdda14c1bedad161d49219910
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-02-07 22:03:00 -0800

    Update lewitt-19.js

--- lewitt-19.js
+++ lewitt-19.js
@@ -7,12 +7,10 @@ for (var i = 0; i <= 550; i = i+5) {
 	quad (140-i, 0-i, 140-i, 0, 140, i, 140+i, 0)
 }
 
-
 for (var i = 0; i <= 140; i = i+5) {
   line (i, 0, i, 340);
 }
 
 for (var i = 0; i <= 340; i = i+5) {
   line (140, i, 340, i);
-
 }
