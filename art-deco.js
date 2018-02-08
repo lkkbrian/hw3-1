e7949c0fa0e503045cd5d6c304488da3c1eb49e1
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-02-07 19:44:39 -0800

    Create art-deco.js

---
+++ art-deco.js
@@ -0,0 +1,8 @@
+for (var i = 0; i <= 20; i = i+5) {
+  noFill();
+  rect (50-i, 170-i, i+i, i+i);
+  rect (110-i, 170-i, i+i, i+i);
+  rect (170-i, 170-i, i+i, i+i);
+  rect (230-i, 170-i, i+i, i+i);
+  rect (290-i, 170-i, i+i, i+i);
+}
