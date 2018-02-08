309da471ee8b98e03958ced01223fdcba04be561
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-02-07 19:57:50 -0800

    Create circle-grid.js

---
+++ circle-grid.js
@@ -0,0 +1,24 @@
+function setup(){
+  createCanvas(340, 340);
+  background (0);
+}
+
+setup();
+for (var i = 20; i < 340; i = i+20) {
+  ellipse (i, 20, 15, 15);
+  ellipse (i, 40, 15, 15);
+  ellipse (i, 60, 15, 15);
+  ellipse (i, 80, 15, 15);
+  ellipse (i, 100, 15, 15);
+  ellipse (i, 120, 15, 15);
+  ellipse (i, 140, 15, 15);
+  ellipse (i, 160, 15, 15);
+  ellipse (i, 180, 15, 15);
+  ellipse (i, 200, 15, 15);
+  ellipse (i, 220, 15, 15);
+  ellipse (i, 240, 15, 15);
+  ellipse (i, 260, 15, 15);
+  ellipse (i, 280, 15, 15);
+  ellipse (i, 300, 15, 15);
+  ellipse (i, 320, 15, 15);
+}
