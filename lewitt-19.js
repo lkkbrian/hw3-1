e26d8358943c0f52e2409f81cddab4bd0fd005ec
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-02-07 22:02:27 -0800

    Create lewitt-19.js

---
+++ lewitt-19.js
@@ -0,0 +1,18 @@
+function setup (){
+  createCanvas (340, 340);
+}
+
+for (var i = 0; i <= 550; i = i+5) {
+  noFill()
+	quad (140-i, 0-i, 140-i, 0, 140, i, 140+i, 0)
+}
+
+
+for (var i = 0; i <= 140; i = i+5) {
+  line (i, 0, i, 340);
+}
+
+for (var i = 0; i <= 340; i = i+5) {
+  line (140, i, 340, i);
+
+}
